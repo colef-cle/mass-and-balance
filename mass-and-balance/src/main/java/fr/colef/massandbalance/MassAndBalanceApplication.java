009548796bfd7d6294cc56319c5541567ee88e98
Colin Lefebvre <cle@colef.fr>
2021-11-21 19:40:40 +0100

    init

---
+++ mass-and-balance/src/main/java/fr/colef/massandbalance/MassAndBalanceApplication.java
@@ -0,0 +1,13 @@
+package fr.colef.massandbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassAndBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassAndBalanceApplication.class, args);
+	}
+
+}
